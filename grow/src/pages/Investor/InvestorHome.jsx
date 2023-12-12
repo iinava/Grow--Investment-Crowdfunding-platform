1e3dfae83961788b1a1a8a32497e173a01e9ed09
iinava <131179814+iinava@users.noreply.github.com>
2023-12-12 11:34:37 +0530

    update
    added filter

--- grow/src/pages/Investor/InvestorHome.jsx
+++ grow/src/pages/Investor/InvestorHome.jsx
@@ -8,26 +8,42 @@ import { Toaster } from 'react-hot-toast'
 export default function InvestorHome() {
   const navigate = useNavigate();
   const [first, setfirst] = useState({})
-  useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/allprojectview").then((response) => {
-      console.log("here comes data");
-      console.log(response);
-      setfirst(response.data.data)
+  const [filter, setFilter] = useState('all');
 
-    }).catch((err) => {
-      console.log(err);
-    })
-  }, [])
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/allprojectview")
+      .then((response) => {
+        console.log("here comes data");
+        console.log(response);
+        setfirst(response.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleFilter = (status) => {
+    setFilter(status);
+    console.log("itahnnnnnnnnn", status)
+  };
+
+  const filteredProjects = filter === 'live'
+  ? first.filter((project) => project.projectstatus === '0')
+  : filter === 'completed'
+  ? first.filter((project) => project.projectstatus === '1')
+  : first;
+
+
   return (
     <div>
-<Toaster/>
+      <Toaster />
       <div className="section-navbar">     <Usernav /></div>
       {/* <div className='gap'>
         <br /><br /><br /> <br /></div> */}
-        <div className="welcomsection adi">
-                <h2>Launching Dreams <span className='coma'>$$$</span> Building Legacies</h2>
-             
-            </div>
+      <div className="welcomsection adi">
+        <h2>Launching Dreams <span className='coma'>$$$</span> Building Legacies</h2>
+
+      </div>
       <div className="homeinformation">
         <h1 className='heading-homeinfo'>Funded Projects</h1>
         <div className='infohomeinfo'>
@@ -47,20 +63,42 @@ export default function InvestorHome() {
         </div>
 
       </div>
+
+
+
+
+      <div className='filterbuttons'>
+        <div
+          className={`buttonone ${filter === 'all' ? 'active' : ''}`}
+          onClick={() => handleFilter('all')}
+        >
+          All
+        </div>
+        <div
+          className={`buttonone ${filter === 'live' ? 'active' : ''}`}
+          onClick={() => handleFilter('live')}
+        >
+          Live
+        </div>
+        <div
+          className={`buttonone ${filter === 'completed' ? 'active' : ''}`}
+          onClick={() => handleFilter('completed')}
+        >
+          Completed
+        </div>
+      </div>
       <div className="cardcollection">
-        {first[0] ?
+        {filteredProjects[0] ? (
           <>
-
-
-            {first?.map((data, key) => (
+            {filteredProjects.map((data, key) => (
               <div className="singlecard">
                 <img src={`/funding/${data.backimage}`} alt="" className="imagesec" />
                 <div className="headingsec">{data.projectname} </div>
                 <div className="pricesec"> sector : {data.sector} </div>
-              
+
                 <div className="pricesec">{data.initialamount}/{data.amount} Dollar </div>
                 <div className="parasec"> {data.briefdesc}</div>
-               
+
                 <div className="viewsec"><button class="buff" onClick={() => { navigate(`/project/${data.id}`) }}>
                   <svg class="svgIcon" viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm50.7-186.9L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg>
                   View
@@ -69,9 +107,10 @@ export default function InvestorHome() {
                 </div>
               </div>
             ))}
-          </>
-          : <td>loading...................</td>
-        }
+            </>
+          ) : (
+            <td>loading...................</td>
+          )}
 
       </div>
 
